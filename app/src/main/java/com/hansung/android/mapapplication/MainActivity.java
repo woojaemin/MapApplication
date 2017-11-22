b4cd96c24310138d53b92a655412ce227539fdbb
woojaemin <31841078+woojaemin@users.noreply.github.com>
2017-11-22 11:58:51 +0900

    ss
    s

---
+++ app/src/main/java/com/hansung/android/mapapplication/MainActivity.java
@@ -0,0 +1,86 @@
+package com.hansung.android.mapapplication;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback{
+
+
+    final int REQUEST_PERMISSIONS_FOR_LAST_KNOWN_LOCATION = 0;
+    private FusedLocationProviderClient mFusedLocationClient;
+    private Location mCurrentLocation;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        if (!checkLocationPermissions()) {
+            requestLocationPermissions(REQUEST_PERMISSIONS_FOR_LAST_KNOWN_LOCATION);
+        } else {
+            getLastLocation();
+        }
+    }
+
+    private boolean checkLocationPermissions() {
+        int permissionState = ActivityCompat.checkSelfPermission(getApplicationContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION);
+        return permissionState == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void requestLocationPermissions(int requestCode) {
+        ActivityCompat.requestPermissions(
+                MainActivity.this,            // MainActivity 액티비티의 객체 인스턴스를 나타냄
+                new String[]{Manifest.permission.ACCESS_FINE_LOCATION},        // 요청할 권한 목록을 설정한 String 배열
+                requestCode    // 사용자 정의 int 상수. 권한 요청 결과를 받을 때
+        );
+    }
+
+    @SuppressWarnings("MissingPermission")
+    private void getLastLocation() {
+        Task task = mFusedLocationClient.getLastLocation();       // Task<Location> 객체 반환
+        task.addOnSuccessListener(this, new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                // Got last known location. In some rare situations this can be null.
+                if (location != null) {
+                    mCurrentLocation = location;
+                       // updateUI();
+                } else {
+                    /*Toast.makeText(getApplicationContext(),
+                            getString(R.string.no_location_detected),
+                            Toast.LENGTH_SHORT)
+                            .show();
+                            */
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        LatLng location = new LatLng(mCurrentLocation.getLatitude(), mCurrentLocation.getLongitude());
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location,15));
+    }
+}
